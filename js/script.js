974c47558b0fbc3967c4c79340166ff9e9f47739
webdevtamim <tamimahmed12.bd@gmail.com>
2023-09-05 09:51:39 +0600

    fourth commit

--- js/script.js
+++ js/script.js
@@ -2,31 +2,37 @@ const loadVideo = async () =>{
     const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
     const data = await res.json();
     const videos = data.data;
-    // console.log(videos);
     displayVideos(videos);
 }
 
 
 const displayVideos = videos =>{
-    // console.log(videos);
+
+    const videoContainer = document.getElementById('video-container')
+
     videos.forEach(video =>{
-        console.log(video);
-        // create a div 
-        const videoCard = document.createElement('div');
-        videoCard.classList = ``;
-        videoCard.innerHTML = `
-            <figure><img src="https://i.ibb.co/L1b6xSq/shape.jpg" class="rounded-lg" alt="" /></figure>
-            <div class="flex gap-3 pt-5">
-                <div>
-                    <img class="w-16 rounded-full" src="https://i.ibb.co/D9wWRM6/olivia.jpg" alt="">
+
+        const author = video.authors
+
+        author.forEach(profile =>{
+
+            const videoCard = document.createElement('div');
+            videoCard.classList = `pb-3`
+            videoCard.innerHTML = `
+                <figure><img src="${video.thumbnail}" class="rounded-lg w-full xl:h-48 md:h-40" /></figure>
+                <div class="flex gap-3 pt-5">
+                    <div>
+                        <img class="w-12 h-3/4 rounded-full" src="${profile.profile_picture}">
+                    </div>
+                    <div>
+                        <h2 class="text-color3 text-base font-bold">${video.title}</h2>
+                        <p class="text-color4 text-sm font-normal pt-3 pb-1">${profile.profile_name}</p>
+                        <p class="text-color4 text-sm font-normal">${video.others.views} views</p>
+                    </div>
                 </div>
-                <div>
-                    <h2 class="text-color3 text-base font-bold">Building a Winning UX Strategy Using the Kano Model</h2>
-                    <p class="text-color4 text-sm font-normal pt-3 pb-1">Awlad Hossain</p>
-                    <p class="text-color4 text-sm font-normal">91K views</p>
-                </div>
-            </div>
-        `
+            `;
+            videoContainer.appendChild(videoCard)
+        })
     })
 }
 
